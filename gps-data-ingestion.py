0142a629c66c45388a5090faa18e59d5a710992b
Arshad Husain <arshad.husain1@gmail.com>
2022-08-26 20:24:14 -0600

    Add ability to write gps data to CSV file

--- gps-data-ingestion.py
+++ gps-data-ingestion.py
@@ -1,16 +1,29 @@
 from flask import Flask, request, Response
+import datetime
+import csv
 
 app = Flask(__name__)
 
+column_names = ['latitude', 'longitude', 'altitude', 'hdop', 'datetime']
 
 @app.route('/ingest_gps', methods=['POST'])
 def door_sensor_change():
-    lat = request.args.get('lat')
-    long = request.args.get('long')
-    altitude = request.args.get('altitude')
-    hdop = request.args.get('hdop')
-    
     try:
+        lat = request.args.get('lat')
+        long = request.args.get('long')
+        altitude = request.args.get('altitude')
+        hdop = request.args.get('hdop')
+
+        gps_data = {}
+        gps_data['latitude'] = request.args.get('lat')
+        gps_data['longitude'] = request.args.get('long')
+        gps_data['altitude'] = request.args.get('altitude')
+        gps_data['hdop'] = request.args.get('hdop')
+        gps_data['datetime'] = datetime.datetime.now()
+
+        with open('gps_data.csv', 'a') as csv_file:
+            dict_object = csv.DictWriter(csv_file, fieldnames=column_names)
+            dict_object.writerow(gps_data)
 
         return Response('OK', status=200, mimetype='text/html')
 
